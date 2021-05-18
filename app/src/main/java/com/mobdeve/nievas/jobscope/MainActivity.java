770e15492f9f6bf2f2df7ce5f9bb547fe50e17ac
iliwysfyasbysuoi <61405165+iliwysfyasbysuoi@users.noreply.github.com>
2021-05-18 11:03:56 +0800

    login page layout :)

--- app/src/main/java/com/mobdeve/nievas/jobscope/MainActivity.java
+++ app/src/main/java/com/mobdeve/nievas/jobscope/MainActivity.java
@@ -2,6 +2,7 @@ package com.mobdeve.nievas.jobscope;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,6 +10,31 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        /*
+            TODO conditional for login/homepage
+            there will be some sort of conditional here.
+            If user is not logged in, go to LoginActivity
+            else, go to homepage of employer
+
+            plan: implement in shared preferences if the user is logged in.
+            when login is successful, save in sharedpreferences.
+            when user logs out, delete login data in shared preferences.
+         */
+
+        // goes to LoginActivity
+        Intent intentLogin = new Intent(this,  LoginActivity.class);
+        startActivity(intentLogin);
+
+        // TODO codes for going to the employer's homepage
+
+
+
+
+
+
     }
+
+
+
 }
